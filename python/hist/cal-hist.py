a1373cff5f3a9000077f85470aa65f7187f2bef5
Kumail Habib Mirza <kumailhabib12@gmail.com>
2021-04-29 23:26:47 +0500

    can view single image histogram

--- python/hist/cal-hist.py
+++ python/hist/cal-hist.py
@@ -16,22 +16,35 @@ imgToLoad = args["image"]
 img2ToLoad = args["image2"]
 
 # init subplots
-rows, cols = 2, 2
+if img2ToLoad:
+    rows, cols = 2, 2
+else:
+    # only show 1 image with its histogram
+    rows, cols = 1, 2
 fig, axs = plt.subplots(rows, cols)
 fig.tight_layout()
 fig.suptitle("Comparsion Using Histogram")
 
 # loading image in grayscale mode
 img = cv.imread(imgToLoad, 0)
-img2 = cv.imread(img2ToLoad, 0)
 
-imgs = [img, img2]
-
+# read second image if given
+if img2ToLoad:
+    img2 = cv.imread(img2ToLoad, 0)
+    imgs = [img, img2]
+else:
+    imgs = [img]
+    
 counter = 0
-for i in range(0, len(imgs)):
-    axs[i, 0].axis("off")
-    axs[i, 0].imshow(imgs[i], cmap='gray')
-    axs[i, 1].hist(imgs[i].ravel(), 256, [0, 255])
- 
+# if 2nd image is given for comparision
+if img2ToLoad:
+    for i in range(0, len(imgs)):
+        axs[i, 0].axis("off")
+        axs[i, 0].imshow(imgs[i], cmap='gray')
+        axs[i, 1].hist(imgs[i].ravel(), 256, [0, 255])
+else: # if only there is 1 image for histogram calc
+    axs[0].axis("off")
+    axs[0].imshow(img, cmap='gray')
+    axs[1].hist(img.ravel(), 256, [0, 255])
 
 plt.show()
